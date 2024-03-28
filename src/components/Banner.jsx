67f93089181b4782bfc8d428949518fd39741194
imranhwebdev <alimrancv870@gmail.com>
2024-03-29 01:10:53 +0600

    fixing done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import bannerTopImg from "../assets/img/heroTopImg.png";
 import btnImg from "../assets/img/btnImg.png";
 import workintImg from "../assets/img/workintImg.png";
+import commentLeft from "../assets/img/comment-left.png";
+import commentRight from "../assets/img/comment-right.png";
 import CopyToClipboard from './CopyToClipboard';
 
 export default function Banner() {
   return (
     <section className='banner-area'>
       <div className="container">
+        <figure className='commentLeft'>
+          <img src={commentLeft} alt="" />
+        </figure>
+        <figure className='commentRight'>
+          <img src={commentRight} alt="" />
+        </figure>
         <div className="banner-content">
             <div className="banner-topImg">
                 <p>Boi iz happy</p>
